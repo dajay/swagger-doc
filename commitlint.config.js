bd248b82006b4979845184ebfb4a5ef4a70cf213
dej <david.jay@redfroggy.fr>
2021-06-30 11:09:43 +0200

    add tools to Release

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,8 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'subject-case': [
-            2, 'always',
+            2,
+            'always',
             ['sentence-case', 'start-case', 'pascal-case', 'lower-case']
         ]
     }
